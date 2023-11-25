b756acee3333f6ca3f18bcdbb2eaf4fc0bbe5a27
Aditya Nugraha <adityanugraha795@gmail.com>
2023-11-25 14:10:32 +0700

    up

---
+++ tes.php
@@ -0,0 +1,30 @@
+
+<br>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hari Guru Nasional 2023</title>
+    <!-- Tambahkan link ke Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <meta name="google" content="notranslate">
+</head>
+<body>
+    
+
+<?php
+
+ echo "<script>alert('Selamat hari guru nasional tahun 2023, semoga hadiahnya bermanfaat yaa')</script>";      
+          echo "<script>window.location = 'https://sppay.shopee.co.id/angbao/GmXotzksuGMboQ6fIdh4LnIIB3yMuKap'</script>"; 
+?>
+
+   
+
+    <!-- Tambahkan script untuk Bootstrap JS (opsional) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
